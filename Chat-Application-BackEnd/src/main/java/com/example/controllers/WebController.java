fb8620333a3bc40f9ef4e2a5f71f2c792d678942
Arby <aegudes@gmail.com>
2022-08-11 10:48:10 -0500

    adds backend and frontend for websocket demo

---
+++ Chat-Application-BackEnd/src/main/java/com/example/controllers/WebController.java
@@ -0,0 +1,17 @@
+package com.example.controllers;
+
+import com.example.models.Hello;
+import com.example.models.User;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/hi")
+    public Hello greeting(User user) throws Exception {
+        return new Hello("Hi" + user.getName() + "!");
+    }
+}
